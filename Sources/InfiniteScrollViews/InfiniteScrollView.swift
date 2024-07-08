c0580d06ed674b9c6711d2a79db65fc9c6b4ebbc
Antoine Bollengier <44288655+b5i@users.noreply.github.com>
2024-07-08 13:01:49 +0200

    Fixed background color appearing in subviews.

--- Sources/InfiniteScrollViews/InfiniteScrollView.swift
+++ Sources/InfiniteScrollViews/InfiniteScrollView.swift
@@ -548,8 +548,7 @@ public class UIInfiniteScrollView<ChangeIndex>: UIScrollView, UIScrollViewDelega
     private func insertView() -> UIView {
         let view = content(changeIndex)
         view.frame = self.contentFrame(changeIndex)
-        //view.layer.borderColor = .init(red: 1, green: 1, blue: 1, alpha: 1)
-        //view.layer.borderWidth = 3
+        view.backgroundColor = .clear
         self.addSubview(view)
         return view
     }
